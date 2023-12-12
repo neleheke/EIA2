88120abd9e6cf0ec6723e552a77c2af28d25418e
neleheke <nele.kempke@gmail.com>
2023-12-12 15:19:28 +0100

    animation

---
+++ AFrame_2/script.js
@@ -0,0 +1,27 @@
+const object = [
+    {src: "./objects/pawn.obj",
+     animation: "property: rotation; to: 0 360 0; loop: true;"},
+    {src: "./objects/rook.obj",
+    animation: "property: rotation; to: 0 0 360; loop: true;"},
+    {src: "./objects/knight.obj",
+    animation: "property: rotation; to: 0 0 360; loop: true;"},
+    {src: "./objects/bishop.obj",
+    animation: "property: rotation; to: 0 0 360; loop: true;"},
+    {src: "./objects/queen.obj",
+    animation: "property: rotation; to: 0 0 360; loop: true;"}
+]
+
+let index = 1
+
+document.addEventListener("click", function() {
+
+    document.getElementById("obj").setAttribute("src", object[index].src);
+    document.getElementById("obj").setAttribute("animation", object[index].animation);
+    index++;
+    
+    if (index == 5) {
+        index = 0
+    }
+
+})
+
